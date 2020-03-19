c2acc102af3e1b3f056fff880f3cfd3d6fd9d5ee
IlonaSoroko <ilona.rumak7@gmail.com>
2020-03-20 00:25:16 +0300

    feat: add solution of Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+  let close = [];
+  let exclusiveElements = [];
+  let stack = [];
+  let openIndex;
+  let closeIndex;
+  str = str.split('');
+
+  bracketsConfig.forEach(element => {
+    if (element[0] == element[1]) {
+      exclusiveElements.push(element[0]);
+    }
+    else {
+      open.push(element[0]);
+      close.push(element[1]);
+    }
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    if (exclusiveElements.includes(str[i])) {
+      stack.push(str[i]);
+      continue;
+    }
+    openIndex = open.indexOf(str[i]);
+    if (openIndex !== -1) {
+      stack.push(openIndex);
+      continue;
+    }
+
+    closeIndex = close.indexOf(str[i]);
+    if (closeIndex !== -1) {
+      openIndex = stack.pop();
+      if (openIndex !== closeIndex) {
+        return false;
+      }
+    }
+  }
+
+
+
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
